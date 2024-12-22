cd1fc176639da7abe21feca67e7bbf6ae7463e4e
EchoTheDeveloper <admin@echothedeveloper.com>
2024-12-22 01:13:27 +0000

    Initial commit

---
+++ GMMLauncher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive;
+using ReactiveUI;
+using System.Threading.Tasks;
+
+namespace GMMLauncher.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        private string DocumentationURL = "https://github.com/EchoTheDeveloper/Goblin-Mod-Maker/blob/main/DOCUMENTATION.md";
+        
+        public ReactiveCommand<Unit, Unit> OpenDocumentationCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            // Initialize the command with an action
+            OpenDocumentationCommand = ReactiveCommand.CreateFromTask(OpenDocumentationAsync);
+        }
+        
+        private async Task OpenDocumentationAsync()
+        {
+            await Task.Run(() =>
+            {
+                Process.Start(new ProcessStartInfo(DocumentationURL) { UseShellExecute = true });
+            });
+        }
+    }
+}
