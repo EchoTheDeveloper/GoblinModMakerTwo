60c99806847382d74888cd99cb10fb75ae8d8a98
EchoTheDeveloper <admin@echothedeveloper.com>
2024-12-22 14:51:31 +0000

    I tried fixing stuff

--- GMMLauncher/ViewModels/MainWindowViewModel.cs
+++ GMMLauncher/ViewModels/MainWindowViewModel.cs
@@ -7,36 +7,39 @@ using Avalonia.Threading;
 
 namespace GMMLauncher.ViewModels
 {
+    // TODO: This keeps crashing giving an async error but its being called from the correct thread
+
     public partial class MainWindowViewModel : ViewModelBase
     {
         private readonly string DocumentationURL = "https://github.com/EchoTheDeveloper/Goblin-Mod-Maker/blob/main/DOCUMENTATION.md";
-        
+
         public ReactiveCommand<Unit, Unit> OpenDocumentationCommand { get; }
         public ReactiveCommand<Unit, Unit> QuitAppCmd { get; }
 
         public MainWindowViewModel()
         {
-            OpenDocumentationCommand = ReactiveCommand.Create(OpenDocumentation);
-            QuitAppCmd = ReactiveCommand.Create(QuitApp);
+            OpenDocumentationCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                await Dispatcher.UIThread.InvokeAsync(OpenDocumentation);
+            }, outputScheduler: RxApp.MainThreadScheduler);
+
+            QuitAppCmd = ReactiveCommand.CreateFromTask(async () =>
+            {
+                await Dispatcher.UIThread.InvokeAsync(QuitApp);
+            }, outputScheduler: RxApp.MainThreadScheduler);
         }
 
         private void OpenDocumentation()
         {
-   
-            // Making sure it runs on the ui thread but at this point idk what the fuck the ui thread is and my mental brain is draining
-            Dispatcher.UIThread.Invoke(() =>
+            Process.Start(new ProcessStartInfo
             {
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = DocumentationURL,
-                    UseShellExecute = true
-                });
+                FileName = DocumentationURL,
+                UseShellExecute = true
             });
         }
 
         private void QuitApp()
         {
-            // TODO: There should be an avalonia shutdown but I could not find it I think they changed it so for now i exit the whole env but I think we should use the Avalonia shutdown aswell
             Environment.Exit(0);
         }
     }
