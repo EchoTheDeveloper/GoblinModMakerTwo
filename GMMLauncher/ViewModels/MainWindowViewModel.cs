c5a65dd313a8e184948255fc8fcc05b9f92334c9
EchoTheDeveloper <admin@echothedeveloper.com>
2024-12-22 01:24:29 +0000

    made exit button work

--- GMMLauncher/ViewModels/MainWindowViewModel.cs
+++ GMMLauncher/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Reactive;
 using ReactiveUI;
 using System.Threading.Tasks;
+using Avalonia;
 
 namespace GMMLauncher.ViewModels
 {
@@ -11,11 +12,13 @@ namespace GMMLauncher.ViewModels
         private string DocumentationURL = "https://github.com/EchoTheDeveloper/Goblin-Mod-Maker/blob/main/DOCUMENTATION.md";
         
         public ReactiveCommand<Unit, Unit> OpenDocumentationCommand { get; }
+        public ReactiveCommand<Unit, Unit> QuitAppCmd { get; }
 
         public MainWindowViewModel()
         {
             // Initialize the command with an action
             OpenDocumentationCommand = ReactiveCommand.CreateFromTask(OpenDocumentationAsync);
+            QuitAppCmd = ReactiveCommand.CreateFromTask(QuitAsync);
         }
         
         private async Task OpenDocumentationAsync()
@@ -23,6 +26,14 @@ namespace GMMLauncher.ViewModels
             await Task.Run(() =>
             {
                 Process.Start(new ProcessStartInfo(DocumentationURL) { UseShellExecute = true });
+            });
+        }
+
+        private async Task QuitAsync()
+        {
+            await Task.Run(() =>
+            {
+                Environment.Exit(0);
             });
         }
     }
