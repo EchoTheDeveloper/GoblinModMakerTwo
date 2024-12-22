a6f0d7f6ed1508012cbfee9ec2da35e746d1419c
EchoTheDeveloper <admin@echothedeveloper.com>
2024-12-22 01:13:27 +0000

    Initial commit

---
+++ GMMBackend/Program.cs
@@ -0,0 +1,6 @@
+﻿namespace GMMBackend {
+    class Program
+    {
+        // test
+    }
+}
---
+++ GMMLauncher/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GMMLauncher.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
