a83e6bf999bdabd77836f27ad4e480065bc51668
EchoTheDeveloper <admin@echothedeveloper.com>
2024-12-22 01:24:29 +0000

    made exit button work

--- GMMLauncher/obj/Debug/net9.0/GMMLauncher.AssemblyInfo.cs
+++ GMMLauncher/obj/Debug/net9.0/GMMLauncher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GMMLauncher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b8a1c3de1df26899ee507953e28fba91eb35a09")]
 [assembly: System.Reflection.AssemblyProductAttribute("GMMLauncher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GMMLauncher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
