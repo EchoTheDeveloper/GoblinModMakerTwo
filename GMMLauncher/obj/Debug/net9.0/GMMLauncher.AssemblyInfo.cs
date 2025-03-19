88ee43c840a21415abff1158c912fa36883eee11
Spiral <burlingamenick190@gmail.com>
2025-03-19 15:42:59 -0400

    stuff

--- GMMLauncher/obj/Debug/net9.0/GMMLauncher.AssemblyInfo.cs
+++ GMMLauncher/obj/Debug/net9.0/GMMLauncher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GMMLauncher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+463fdec408900b63fec378be7f5cd508092df7a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4d033641d9efe5f71291591333dcf84cd36bb3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GMMLauncher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GMMLauncher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
