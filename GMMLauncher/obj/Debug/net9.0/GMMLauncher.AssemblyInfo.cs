360bc19f0aa19ca5de4b4dea688402135470da8f
Spiral <burlingamenick190@gmail.com>
2025-03-20 23:29:20 -0400

    streamjsonrpc gone

--- GMMLauncher/obj/Debug/net9.0/GMMLauncher.AssemblyInfo.cs
+++ GMMLauncher/obj/Debug/net9.0/GMMLauncher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GMMLauncher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2de4aedbcc321f76a1ec5ed7fa8909cc10ada2af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ae293947d56ff9a7bd193fff1c3d8de4d4d97d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GMMLauncher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GMMLauncher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
